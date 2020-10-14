33e3e6056764e456a8b13d537eecff58edca4dd7
han1202012 <octopus_truth@163.com>
2020-10-14 19:39:56 +0800

    update

--- app/src/main/java/kim/hsl/oboedemo/MainActivity.kt
+++ app/src/main/java/kim/hsl/oboedemo/MainActivity.kt
@@ -10,18 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
         sample_text.text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     external fun stringFromJNI(): String
 
     companion object {
-        // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
         }
